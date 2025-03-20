00b3a80f825f52bdb358b140a3ba06f725bc3fc7
Matio004 <mateuszkupis@interia.eu>
2025-03-20 14:56:09 +0100

    des .1%

---
+++ src/main/java/org/example/Utils.java
@@ -0,0 +1,51 @@
+package org.example;
+
+public class Utils {
+
+    public static byte[] selectBits(byte[] array, byte[] bits) {
+        byte[] bytes = new byte[bits.length/8];
+
+        for (int i = 0; i < bits.length; i++) {
+            setBit(bytes, i, isBitSet(array, bits[i]));
+        }
+        return bytes;
+    }
+
+    public static boolean isBitSet(byte b, int n) {
+        return (b & (1 << n)) != 0;
+    }
+
+    public static boolean isBitSet(byte[] bytes, int n) {
+        return isBitSet(bytes[n / 8], n % 8);
+    }
+
+    public static void setBit(byte[] bytes, int n, boolean value) {
+        int posByte = n / 8;
+        int postBit = n % 8;
+        bytes[posByte] = setBit(bytes[posByte], postBit, value);
+    }
+
+    public static byte setBit(byte b, int n, boolean value) {
+        if (value) {
+            return (byte) (b | (1 << n));
+        }
+        return (byte) (b & ~(1 << n));
+    }
+
+    public static byte[] rotateLeft(byte[] array, int n) {
+        byte[] temp = array.clone();
+
+        for (int i = n; i < array.length * 8 + n; i++) {
+            setBit(temp, i - n, isBitSet(array, i % (array.length * 8)));
+        }
+        return temp;
+    }
+
+    public static byte[] connectHalves(byte[] h0, byte[] h1) {
+        byte[] temp = new byte[h0.length + h1.length];
+
+        System.arraycopy(h0, 0, temp, 0, h0.length);
+        System.arraycopy(h1, 0, temp, h0.length, h1.length);
+        return temp;
+    }
+}
