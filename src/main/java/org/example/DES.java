265a79eb02192fe68d3914ba1469f515e01a80bb
Matio004 <mateuszkupis@interia.eu>
2025-03-31 22:14:19 +0200

    des premutations

--- src/main/java/org/example/DES.java
+++ src/main/java/org/example/DES.java
@@ -58,6 +58,28 @@ public class DES {
             1, 7, 23, 13, 31, 26, 2, 8, 18, 12, 29, 5, 21, 10, 3, 24
     };
 
+    private final byte[] ip = {
+            57, 49, 41, 33, 25, 17, 9, 1,
+            59, 51, 43, 35, 27, 19, 11, 3,
+            61, 53, 45, 37, 29, 21, 13, 5,
+            63, 55, 47, 39, 31, 23, 15, 7,
+            56, 48, 40, 32, 24, 16, 8, 0,
+            58, 50, 42, 34, 26, 18, 10, 2,
+            60, 52, 44, 36, 28, 20, 12, 4,
+            62, 54, 46, 38, 30, 22, 14, 6
+    };
+
+    private final byte[] ipInv = {
+            39, 7, 47, 15, 55, 23, 63, 31,
+            38, 6, 46, 14, 54, 22, 62, 30,
+            37, 5, 45, 13, 53, 21, 61, 29,
+            36, 4, 44, 12, 52, 20, 60, 28,
+            35, 3, 43, 11, 51, 19, 59, 27,
+            34, 2, 42, 10, 50, 18, 58, 26,
+            33, 1, 41, 9, 49, 17, 57, 25,
+            32, 0, 40, 8, 48, 16, 56, 24
+    };
+
     public DES(byte[] key) {
         this.key = key;
         generateSubKeys();
@@ -122,7 +144,9 @@ public class DES {
         return out;
     }
 
-    private byte[] encodeBlock(byte[] bytes) {
+    public byte[] encodeBlock(byte[] bytes) {
+        bytes = Utils.selectBits(bytes, ip);
+
         byte[] r = rightBlock(bytes);
         byte[] l = leftBlock(bytes);
 
@@ -136,10 +160,11 @@ public class DES {
             l = temp;
         }
 
-        return Utils.connectHalves(r, l);
+        return Utils.selectBits(Utils.connectHalves(r, l), ipInv);
     }
 
-    private byte[] decodeBlock(byte[] bytes) {
+    public byte[] decodeBlock(byte[] bytes) {
+        bytes = Utils.selectBits(bytes, ip);
         byte[] r = rightBlock(bytes);
         byte[] l = leftBlock(bytes);
 
@@ -153,7 +178,7 @@ public class DES {
             l = temp;
         }
 
-        return Utils.connectHalves(r, l);
+        return Utils.selectBits(Utils.connectHalves(r, l), ipInv);
     }
 
     public byte[] encode(byte[] bytes) {
